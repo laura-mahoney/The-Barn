c6d25c8c2e85f29fe5f8d5cef4f2e6eb1aaeeb56
Laura Mahoney <lauramahoney@me.com>
2024-07-11 12:16:17 -0700

    updates to pupdates at the barn

--- fblogin.py
+++ fblogin.py
@@ -8,11 +8,11 @@ facebook = OAuth2Session(app_id, redirect_uri=redirect_uri)
 facebook = facebook_compliance_fix(facebook)
 
 authorization_url, state = facebook.authorization_url(authorization_base_url)
-print 'Please authorize.', authorization_url
+print('Please authorize.', authorization_url)
 
 redirect_response = raw_input("https://localhost:5000")
 
 facebook.fetch_token(token_url, app_secret=app_secret, authorization_response=redirect_response)
 
 r = facebook.get('https://graph.facebook.com/me?')
-print r.content
+print(r.content)
--- seed.py
+++ seed.py
@@ -103,13 +103,109 @@ def load_shift():
 if __name__ == "__main__":
     connect_to_db(app)
 
+    with app.app_context():
+        db.create_all()
+        
+        load_shift()
+        load_kennel()
+        load_positions()
+        load_crew()
+        # set_val_crew_id()
+        load_dogs()
+
+    # db.create_all()
     
-    db.create_all()
+    # load_shift()
+    # load_kennel()
+    # load_positions()
+    # load_crew()
+    # # set_val_crew_id()
+    # load_dogs()
     
-    load_shift()
-    load_kennel()
-    load_positions()
-    load_crew()
-    # set_val_crew_id()
-    load_dogs()
+# """utility file to seed barncrew and dogs"""
+
+# from sqlalchemy import func
+# from model import Barncrew, Dog, Kennel, Positions, Shift
+# import datetime
+
+# from model import connect_to_db, db
+# from server import app
+
+# def load_crew():
+#     """Load crew from database"""
+#     Barncrew.query.delete()
+
+#     # Read barncrew file and insert data
+#     for row in open("seed_data/barncrew.txt"):
+#         row = row.rstrip()
+#         fname, lname, email, phone, password, pos_code = row.split("|")
+
+#         crew = Barncrew(fname=fname, lname=lname, email=email, phone=phone, password=password, pos_code=pos_code)
+        
+#         db.session.add(crew)
+ 
+#     db.session.commit()
+
+# def load_dogs():
+#     Dog.query.delete()
+
+#     for row in open("seed_data/dogs.txt"):
+#         row = row.strip()
+#         dog_name, kennel_id, breed, gender, altered, age, intake_date, dog_pic = row.split("|")
+
+#         if intake_date:
+#             intake_date = datetime.datetime.strptime(intake_date, "%d-%b-%Y")
+#         else:
+#             intake_date = None
+
+#         dog = Dog(dog_name=dog_name, kennel_id=kennel_id, breed=breed, gender=gender, altered=altered, age=age, intake_date=intake_date, dog_pic=dog_pic)
+
+#         db.session.add(dog)
+
+#     db.session.commit()
+
+# def load_positions():
+#     Positions.query.delete()
+
+#     pos = ['mgmt', 'admin', 'volunteer']
+
+#     for p in pos: 
+#         position = Positions(pos_code=p)
+#         db.session.add(position)
+
+#     db.session.commit()
+
+# def load_kennel():
+#     Kennel.query.delete()
     
+#     for row in open("seed_data/kennel.txt"):
+#         row = row.strip()
+#         dog_door, size, indoor = row.split("|")
+   
+#         kennel = Kennel(dog_door=dog_door, size=size, indoor=indoor)
+#         db.session.add(kennel)
+
+#     db.session.commit()
+
+# def load_shift():
+#     for row in open("seed_data/shift.txt"):
+#         row = row.strip()
+#         shift_id, date_time, duration, notes = row.split("|") 
+    
+#         shift = Shift(shift_id=shift_id, date_time=date_time, duration=duration, notes=notes)
+#         db.session.add(shift)
+
+#     db.session.commit()
+
+# if __name__ == "__main__":
+#     connect_to_db(app)
+
+#     with app.app_context():
+#         db.create_all()
+        
+#         load_shift()
+#         load_kennel()
+#         load_positions()
+#         load_crew()
+#         # set_val_crew_id()
+#         load_dogs()
